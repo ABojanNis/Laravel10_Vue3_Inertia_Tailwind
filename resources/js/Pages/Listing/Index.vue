cf89cc19a2a0ccec847ccfde6e8b2c8fb07c2063
Bojan <bojan.adamovic@madduckcode.com>
2023-06-13 21:10:09 +0200

    Implemented monthly payment calculator

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,9 +1,5 @@
 <script setup>
-import { Link } from '@inertiajs/vue3'
-import ListingAddress from "@/Components/ListingAddress.vue";
-import Box from "@/Components/UI/Box.vue";
-import ListingSpace from "@/Components/ListingSpace.vue";
-import Price from "@/Components/Price.vue";
+import Listing from "./Index/Components/Listing.vue";
 
 defineProps({
     listings: Array
@@ -12,19 +8,7 @@ defineProps({
 
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <Box v-for="listing in listings" :key="listing.id">
-            <Link :href="route('listing.show', { listing: listing.id })">
-                <Price :price="listing.price" class="text-2xl font-bold"/>
-                <ListingSpace :listing="listing" class="text-lg"/>
-                <ListingAddress :listing="listing" class="text-gray-500"/>
-            </Link>
-            <Link :href="route('listing.edit', { listing: listing.id })">
-                Edit
-            </Link>
-            <Link :href="route('listing.destroy', { listing: listing.id })" method="delete" as="button">
-                Delete
-            </Link>
-        </Box>
+        <Listing v-for="listing in listings" :key="listing.id" :listing="listing"/>
     </div>
 </template>
 
